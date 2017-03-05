d87af672e2b784b4dc152c7aa010f6adf50bb1d8
O'Hoon Kwon <ohkwon@ucdavis.edu>
2017-03-05 15:51:18 -0800

    add blog model with user_id, description, and url

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305234940) do
+ActiveRecord::Schema.define(version: 20170305235056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "description"
+    t.string   "url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
