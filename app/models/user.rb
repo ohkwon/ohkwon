5f29edd572d8993ebaae918b0e1bc4fda2934fa2
O'Hoon Kwon <ohkwon@ucdavis.edu>
2017-03-05 16:58:11 -0800

    remove company joins and redo relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_many :user_skills
   has_many :skills, through: :user_skills
   # add bullet point reference after considering which way to set up
+  # must remove company joins
 end
