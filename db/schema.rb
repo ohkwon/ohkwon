d9cb080ef4ec0cbf832f87d2cb4aea9b1e3f2400
O'Hoon Kwon <ohkwon@ucdavis.edu>
2017-03-08 18:28:41 -0800

    add name to blog model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309020720) do
+ActiveRecord::Schema.define(version: 20170309022803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170309020720) do
     t.string   "url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
   create_table "bullet_points", force: :cascade do |t|
