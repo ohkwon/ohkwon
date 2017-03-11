c79db55f09727f2c3a1d6361159d81c54a82acc1
O'Hoon Kwon <ohkwon@ucdavis.edu>
2017-03-11 10:20:47 -0800

    complete very basics of user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   # this is the going to be the "home" page for the app
   def show
-
+    @user = User.find_by(id: params[:id])
+    @skills = Skill.joins(:user_skills).where("user_skills.user_id = #{params[:id]}")
+    @projects = Project.where(user_id: params[:id])
+    @experiences = Experience.where(user_id: params[:id])
+    @educations = Education.where(user_id: params[:id])
   end
 
   def edit
